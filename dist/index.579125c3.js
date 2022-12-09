b06e521cd6a5efd5ff2f625072fd946ccd544f89
juanrabaro <jrbaro@iesrafaelalberti.es>
2022-12-09 12:19:31 +0100

    dudas y responsive terminado, falta solo las cosas de la lista

---
+++ dist/index.579125c3.js
@@ -0,0 +1,7 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("body-dark-mode");
+    var estaEnDark = "dark";
+}
+
+//# sourceMappingURL=index.579125c3.js.map
---
+++ src/index.js
@@ -0,0 +1,5 @@
+function darkMode(){
+    var element = document.body;
+    element.classList.toggle("body-dark-mode");
+    var estaEnDark = "dark";
+}

1b8c56337bab9e72e2b84967024c89b8f5ea07e5
juanrabaro <jrbaro@iesrafaelalberti.es>
2022-12-09 20:41:16 +0100

    añadiendo dark mode a todas las paginas

--- dist/index.579125c3.js
+++
@@ -1,7 +0,0 @@
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("body-dark-mode");
-    var estaEnDark = "dark";
-}
-
-//# sourceMappingURL=index.579125c3.js.map
--- src/darkMode.js
+++ src/darkMode.js
@@ -1,13 +1,46 @@
-function darkMode(){
+function darkModeHeader(){
+    var botimg = document.getElementById("bot-img-darkMode");
+    botimg.classList.toggle("bot-img-dark-mode");
+}
+
+function darkModeFooter(){
+    
+}
+
+function darkModeIndex(){
     var elementb = document.body;
     elementb.classList.toggle("body-dark-mode");
 
-    var elementh = document.getElementById("header");
-    elementh.classList.toggle("header-dark-mode");
+    var botimg = document.getElementById("bot-img-darkMode");
+    botimg.classList.toggle("bot-img-dark-mode");
 
-    var elementf = document.footer;
-    elementf.classList.toggle("footer-dark-mode");
+    var fieldset1 = document.getElementById("fieldset-darkMode1");
+    fieldset1.classList.toggle("fieldset-dark-mode");
+    var fieldset2 = document.getElementById("fieldset-darkMode2");
+    fieldset2.classList.toggle("fieldset-dark-mode");
+}
 
-    
+function darkModeWiki(){
+    var elementb = document.body;
+    elementb.classList.toggle("body-dark-mode");
+}
 
+function darkModeDescripcion(){
+    var elementb = document.body;
+    elementb.classList.toggle("body-dark-mode");
+}
+
+function darkModeContacto(){
+    var elementb = document.body;
+    elementb.classList.toggle("body-dark-mode");
+}
+
+function darkModePag404(){
+    var elementb = document.body;
+    elementb.classList.toggle("body-dark-mode");
+}
+
+function darkModeUsuario(){
+    var elementb = document.body;
+    elementb.classList.toggle("body-dark-mode");
 }
